15bc23c41abed82d7edf73cc38a22eb4fbb1711c
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2023-11-29 11:40:02 +0000

    APIS-6490: Addressed PR BOT comments

--- build.sbt
+++ build.sbt
@@ -11,9 +11,9 @@ lazy val appName = "third-party-orchestrator"
 lazy val plugins: Seq[Plugins]         = Seq(PlayScala, SbtDistributablesPlugin)
 lazy val playSettings: Seq[Setting[_]] = Seq.empty
 
-scalaVersion := "2.13.8"
+scalaVersion := "2.13.12"
 
-ThisBuild / scalafixDependencies += "com.github.liancheng" %% "organize-imports" % "0.6.0"
+ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 ThisBuild / semanticdbEnabled := true
 ThisBuild / semanticdbVersion := scalafixSemanticdb.revision
 
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,19 +8,23 @@ object AppDependencies {
 
   private val bootstrapVersion = "7.22.0"
   private val enumeratumVersion = "1.7.0"
+  lazy val scalaCheckVersion = "1.15.4"
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
   val compileDeps = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"       % bootstrapVersion,
     "uk.gov.hmrc"             %% "http-metrics"                    % "2.7.0",
-    "com.beachape"            %% "enumeratum"                      % enumeratumVersion,
-    "com.beachape"            %% "enumeratum-play"                 % enumeratumVersion,
     "uk.gov.hmrc"             %% "api-platform-application-domain" % "0.25.0"
   )
 
+   lazy val testScopes = Seq(Test.name, IntegrationTest.name).mkString(",")
+
   val testDeps = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"          % bootstrapVersion,
-    "org.mockito"             %% "mockito-scala-scalatest"         % "1.17.22"
-  ).map(_ % "test, it")
+    "org.mockito"             %% "mockito-scala-scalatest"         % "1.17.22",
+    "org.scalatest"           %% "scalatest"                       % "3.2.17",
+    "com.vladsch.flexmark"    % "flexmark-all"                     % "0.62.2" 
+
+  ).map(_ % testScopes)
 }
