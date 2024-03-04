76c09f0e01d17255c82549d064dc31e06a0f5ca6
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-03-04 10:26:13 +0000

    APIS-6779 - Add events lib (and app domain lib bump) ready for extra work

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "8.4.0"
-  private val appDomainVersion = "0.36.0"
   private val commonDomainVersion = "0.13.0"
+  private val applicationEventVersion  = "0.43.0"
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
   val compileDeps = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30"       % bootstrapVersion,
     "uk.gov.hmrc" %% "http-metrics"                    % "2.8.0",
-    "uk.gov.hmrc" %% "api-platform-application-domain" % appDomainVersion
+    "uk.gov.hmrc" %% "api-platform-application-events" % applicationEventVersion
   )
 
   val testDeps = Seq(
