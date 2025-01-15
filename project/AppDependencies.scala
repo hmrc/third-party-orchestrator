ecf6fd8e5437555dcb5273d700022d0c97b8ce1e
kskourtis1 <kskourtis1@users.noreply.github.com>
2025-01-15 11:46:19 +0000

    APIS-7339: Gatekeeper User Can Set Deleted Restricted - bump api-platform-application-events and api-platform-application-domain-fixtures versions (#35)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ object AppDependencies {
 
   private val bootstrapVersion = "9.1.0"
   private val commonDomainVersion = "0.18.0"
-  private val applicationEventVersion  = "0.69.1"
+  private val applicationEventVersion  = "0.72.0"
   private val tpdDomainVersion  = "0.11.0"
-  private val appDomainVersion = "0.67.0"
+  private val appDomainVersion = "0.69.0"
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
