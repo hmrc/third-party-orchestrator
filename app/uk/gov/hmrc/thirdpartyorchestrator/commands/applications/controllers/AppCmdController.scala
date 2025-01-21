512b066e6ae6d6943098c77b3e16afb419c969a9
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-01-21 14:06:37 +0000

    API-8121 - Add dispatchToEnvironment endpoint

--- app/uk/gov/hmrc/thirdpartyorchestrator/commands/applications/controllers/AppCmdController.scala
+++ app/uk/gov/hmrc/thirdpartyorchestrator/commands/applications/controllers/AppCmdController.scala
@@ -17,17 +17,18 @@
 package uk.gov.hmrc.thirdpartyorchestrator.commands.applications.controllers
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+import scala.concurrent.Future.successful
+import scala.concurrent.{ExecutionContext, Future}
 
 import cats.data.NonEmptyList
 import cats.implicits.catsStdInstancesForFuture
 
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsValue, Json, Reads}
 import play.api.mvc._
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import uk.gov.hmrc.apiplatform.modules.commands.applications.domain.models._
-import uk.gov.hmrc.apiplatform.modules.common.domain.models.ApplicationId
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.{ApplicationId, Environment}
 import uk.gov.hmrc.apiplatform.modules.common.domain.services.NonEmptyListFormatters._
 import uk.gov.hmrc.apiplatform.modules.common.services.EitherTHelper
 import uk.gov.hmrc.thirdpartyorchestrator.commands.applications.connectors.EnvironmentAwareAppCmdConnector
@@ -45,12 +46,28 @@ class AppCmdController @Inject() (
 
   val E = EitherTHelper.make[NonEmptyList[CommandFailure]]
 
+  lazy val badRequestResponse = BadRequest(Json.toJson(NonEmptyList.one[CommandFailure](CommandFailures.ApplicationNotFound)))
+
   def dispatch(id: ApplicationId): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    applicationService.fetchApplication(id)
+      .flatMap(_ match {
+        case Some(app) => commonDispatch(app.deployedTo, app.id)
+        case None      => successful(badRequestResponse)
+      })
+  }
+
+  def dispatchToEnvironment(environment: Environment, id: ApplicationId): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    commonDispatch(environment, id)
+  }
+
+  private def commonDispatch(environment: Environment, id: ApplicationId)(implicit request: Request[JsValue], m: Manifest[DispatchRequest], reads: Reads[DispatchRequest])
+      : Future[Result] = {
     withJsonBody[DispatchRequest] { inboundDispatchRequest =>
-      (for {
-        application    <- E.fromOptionF(applicationService.fetchApplication(id), NonEmptyList.one(CommandFailures.ApplicationNotFound))
-        responseStatus <- E.fromEitherF(cmdConnector(application.deployedTo).dispatch(application.id, inboundDispatchRequest))
-      } yield responseStatus)
+      (
+        for {
+          responseStatus <- E.fromEitherF(cmdConnector(environment).dispatch(id, inboundDispatchRequest))
+        } yield responseStatus
+      )
         .fold(
           failures => BadRequest(Json.toJson(failures)),
           success => Ok(Json.toJson(success))
