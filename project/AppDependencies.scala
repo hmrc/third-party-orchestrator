28d707792ae8c866dd006d89877d9e2c4c894e63
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-09-08 14:10:36 +0100

    API-8591 - Bump app lib version for token

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.17.0"
   private val commonDomainVersion = "0.18.0"
   private val tpdDomainVersion  = "0.13.0"
   
-  private val appDomainVersion = "0.76.0"
-  private val appEventVersion  = "0.80.0" // Ensure this version of the application-events library uses the appDomainVersion above
+  private val appDomainVersion = "0.82.0"
+  private val appEventVersion  = "0.83.0" // Ensure this version of the application-events library uses the appDomainVersion above
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
